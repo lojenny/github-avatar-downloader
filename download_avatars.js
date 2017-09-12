32161f7640d64d4e87cc78ab5690ddd95cd4b96d
lojenny <jennychingyinlo@gmail.com>
2017-09-12 00:38:55 +0000

    add getRepoContributors.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,42 @@
+require('dotenv').config();
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getOptionsForRepo(person, repo){
+  return {
+    url: `https://api.github.com/repos/${person}/${repo}/contributors`,
+    qs: {
+      sort: 'pushed',
+      access_token: process.env.GITHUB_TOKEN
+    },
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+}
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  const options = getOptionsForRepo(repoOwner, repoName);
+
+  request(options, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    data.forEach((contributor) => {
+      console.log(contributor.login, contributor.avatar_url);
+    });
+
+    return data;
+  });
+
+}
+
+
+getRepoContributors("jquery", "jquery", function (err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
